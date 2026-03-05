40a235dd2d9beada4de3c9962115ff0dfcd57765
Noel Noble <99111723+NoelNoble@users.noreply.github.com>
2026-03-05 20:02:18 +0530

    Add files via upload

---
+++ compound-growth-calculator.py
@@ -0,0 +1,29 @@
+w = input("Increase(1) or Decrease(2): ")
+while len(w) == 0:
+   w = input("Increase(1) or Decrease(2): ")
+
+w = int(w)
+if w == 1:
+   x = float(input("The pricipal amount: "))
+
+   y = int(input("The number of iterations (integers only): "))
+
+   z = float(input("Percentage increase: "))
+   z = z/100
+
+   for _ in range(y):
+    x =  x + x*z
+   
+elif w == 2:
+   x = float(input("The pricipal amount: "))
+
+   y = int(input("The number of iterations (integers only): "))
+
+   z = float(input("Percentage decrease: "))
+   z = z/100
+   for _ in range(y):
+      x =  x - x*z
+else:
+   print("Invalid choice")   
+   
+print("The answer is -" , x)
